4d948fd955bcc93175561abbec73984729e41099
Vineel Sai <mail@vineelsai.com>
2024-07-07 20:52:40 +0530

    fix tests

--- tests/python.rs
+++ tests/python.rs
@@ -4,15 +4,7 @@ use vmp::python::{install, list, uninstall, usage};
 async fn test_python_3_12() {
     let _result = install::install_python("3.12".to_string()).await;
     let _result = usage::use_python("3.12".to_string()).await;
-    let _result = uninstall::uninstall_python("3.12".to_string()).await;
-}
-
-#[tokio::test]
-async fn test_python_list_installed() {
     let _result = list::list_python_versions("installed".to_string()).await;
-}
-
-#[tokio::test]
-async fn test_python_list_all() {
     let _result = list::list_python_versions("all".to_string()).await;
+    let _result = uninstall::uninstall_python("3.12".to_string()).await;
 }
